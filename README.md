db30e4e3d92ce448185d1bcb70aab50a47fee1e3
squizu <squizu@gmail.com>
2021-06-11 11:40:46 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Inatitation à Python
+
+## Discord Bot
+*J'ai décidé de découvrir le langgage Python de façon ludique, c'est pourquoi je vais suivre des tutorials traitant des Bots Discord.*
+
+[Source](https://www.writebots.com/how-to-make-a-discord-bot/)
+___
+
+### Découverte d'un bot basique [source](https://discordpy.readthedocs.io/en/latest/quickstart.html#a-minimal-bot)
---
+++ Training/minimal_bot.py
@@ -0,0 +1,17 @@
+import discord # Import the library
+
+client = discord.Client() # Create an instance of a client
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run('your token here')
